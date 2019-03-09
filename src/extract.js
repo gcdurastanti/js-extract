9407d8b1777c6b57e6977f572487e4071f7135e8
Giancarlo Durastanti <gdurastanti@paypal.com>
2019-03-09 00:40:48 -0500

    working commit

---
+++ src/extract.js
@@ -0,0 +1,90 @@
+import _ from 'lodash';
+
+// extend String so we can find the next index of a character
+Object.defineProperty(String.prototype, 'nextIndexOf', {
+  value(character, currentIndex) {
+    const index = this.slice(currentIndex).indexOf(character);
+    return index > 0 ? index + currentIndex : 0;
+  },
+});
+
+const findClosingBracketMatchIndex = (str, pos) => {
+  if (str[pos] != '{') {
+    throw new Error("No '{' at index " + pos);
+  }
+  let depth = 1;
+  for (let i = pos + 1; i < str.length; i++) {
+    switch (str[i]) {
+      case '{':
+        depth++;
+        break;
+      case '}':
+        if (--depth == 0) {
+          return i;
+        }
+        break;
+    }
+  }
+  return -1; // No matching closing bracket
+};
+
+const parseBlock = block => {
+  let selectors = [];
+
+  if (_.isEmpty(block)) {
+    return selectors;
+  } else if (!block.includes(':')) {
+    return block.replace(/({|})/g, '').split(',');
+  }
+
+  const parts = block.split('');
+  for (let i = 0; i < parts.length; i++) {
+    const indices = [
+      { token: ':', index: block.nextIndexOf(':', i) },
+      { token: ',', index: block.nextIndexOf(',', i) },
+      { token: '{', index: block.nextIndexOf('{', i) },
+      { token: '}', index: block.nextIndexOf('}', i) },
+    ];
+    const min = _.minBy(indices.filter(val => val.index > 0), 'index');
+
+    if (min && min.token === ':') {
+      const openBracketIndex = min.index + 1;
+      const topKey = block.substring(i, min.index).replace(',', '');
+      const blockEnd = findClosingBracketMatchIndex(block, openBracketIndex);
+
+      const keys = block.substring(openBracketIndex + 1, blockEnd).split(',');
+
+      const subKeys = parseBlock(keys.filter(key => key.includes(':')).join(','));
+      const mapper = key => `${topKey}.${key}`;
+
+      selectors.push(...keys.map(mapper), ...subKeys.map(mapper));
+
+      // set loop index to end of this block since the recursion has handled it
+      i += blockEnd;
+    } else if (min && min.token === ',') {
+      const selector = block.substring(i, min.index);
+      selectors.push(selector);
+      i += min.index;
+    } else if (!min) {
+      selectors.push(block.substring(i, parts.length).replace(',', ''));
+      break;
+    }
+  }
+
+  return _.flattenDeep(selectors.filter(selector => !selector.includes(':')));
+};
+
+const extract = query => {
+  const normalized = query.replace(/\r?\n|\r|\s/g, '');
+  const selectors = parseBlock(normalized);
+
+  const from = data => ({ selectors, result: _.pick(data, selectors) });
+
+  return {
+    query,
+    selectors,
+    from,
+  };
+};
+
+export default extract;
---
+++ test/extract.test.js
@@ -0,0 +1,67 @@
+import assert from 'assert';
+import extract from '../src/extract';
+
+const data = {
+  1: {
+    first: 'One',
+    second: {
+      two: 'Two',
+      three: 'Three',
+      four: {
+        val: 'Four',
+      },
+    },
+  },
+  2: {
+    first: 'Uno',
+    second: 'Due',
+    third: 'Tre',
+  },
+};
+
+const query = `
+  1: {
+    first,
+    second: {
+      two,
+      four: {
+        val
+      }
+    }
+  },
+  2: {
+    third
+  }
+`;
+
+const filteredData = {
+  1: {
+    first: 'One',
+    second: {
+      two: 'Two',
+      four: {
+        val: 'Four',
+      },
+    },
+  },
+  2: {
+    third: 'Tre',
+  },
+};
+
+describe('extract', () => {
+  it('should return the expecsted shape', () => {
+    const { result } = extract(query).from(data);
+    assert.deepEqual(result, filteredData);
+  });
+
+  it('should return the selected top level keys', () => {
+    const { result } = extract(`1, 2`).from(data);
+    assert.deepEqual(result, data);
+  });
+
+  it('should return the selected top level keys', () => {
+    const { result } = extract(`{1, 2}`).from(data);
+    assert.deepEqual(result, data);
+  });
+});
