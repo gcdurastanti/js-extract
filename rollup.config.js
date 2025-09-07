df8c0d46e3c1fd78802c7b52eca4c61d7ef9fb23
Giancarlo Durastanti <cvd2261@protonmail.com>
2025-09-06 20:52:22 -0400

    Fixed build

--- rollup.config.js
+++ rollup.config.js
@@ -1,7 +1,7 @@
 import babel from '@rollup/plugin-babel';
 
 export default {
-  input: 'src/extract.v3.js',
+  input: 'src/extract.js',
   output: {
     file: 'lib/extract.v3.umd.js',
     format: 'umd',
